bdb58028e9fc32d799b0a589a296ab41301f955a
Free Debreuil <freedebreuil@gmail.com>
2017-12-18 21:56:30 -0800

    Added sprite manager for sorting

---
+++ Assets/Content/Scene Game/Scripts/SCR_SpriteManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SCR_SpriteManager : MonoBehaviour {
+
+	SCR_SpriteManager() {
+		EditorApplication.hierarchyWindowChanged -= MyHierarchyChangedCallback;
+		EditorApplication.hierarchyWindowChanged += MyHierarchyChangedCallback;
+	}
+
+	private static void MyHierarchyChangedCallback() {
+		var sprites = GameObject.FindObjectOfType<SCR_SpriteManager>()
+									.GetComponentsInChildren<SpriteRenderer>();
+		var index = 0;
+		foreach (var item in sprites) {
+			index += 1;
+			item.sortingOrder = index;
+		}
+	}
+
+	// protected void Update () {
+	// 	if (Application.isEditor) {
+	// 		var sprites = GetComponentsInChildren<SpriteRenderer>();
+	// 		var index = 0;
+	// 		foreach (var item in sprites) {
+	// 			index += 1;
+	// 			item.sortingOrder = index;
+	// 		}
+	// 	}
+	// }
+
+}
