eb156dc8577ba4dbeb0fc93fcd67c2a47a46bf36
F286 <free@debreuil.com>
2018-01-04 10:26:38 +0900

    Added simple singleton example

---
+++ Assets/Standard Assets/Core/Scripts/ExampleSingletonSimple.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ExampleSingletonSimple : MonoBehaviour {
+
+  static ExampleSingletonSimple reference;
+  public static ExampleSingletonSimple instance {
+    get {
+      if (reference == null) {
+        reference = new GameObject("Simple Singleton").AddComponent<ExampleSingletonSimple>();
+      }
+      return reference;
+    }
+  }
+}
