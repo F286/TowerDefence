7e967368b71572af510ac5b8ee96eba20fd729a0
F286 <free@debreuil.com>
2018-01-04 10:09:21 +0900

    Added example code for singleton with interface

---
+++ Assets/Standard Assets/Core/Scripts/ExampleSingleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ExampleSingleton {
+  static IExampleSingleton reference;
+  public static IExampleSingleton instance {
+    get {
+      if (reference == null) {
+        if (Application.platform == RuntimePlatform.Android) {
+          reference = new GameObject("Android Example Singleton").AddComponent<ExampleSingletonAndroid>();
+        }
+        else {
+          reference = new GameObject("iOS Example Singleton").AddComponent<ExampleSingletonIOS>();
+        }
+      }
+      return reference;
+    }
+  }
+}
+public interface IExampleSingleton {
+  void AddValue(int number);
+}
+public class ExampleSingletonAndroid : MonoBehaviour, IExampleSingleton {
+  int value;
+  public void AddValue(int number) {
+    value += number * 2;
+  }
+}
+public class ExampleSingletonIOS : MonoBehaviour, IExampleSingleton {
+  int value;
+  public void AddValue(int number) {
+    value += number;
+  }
+}
